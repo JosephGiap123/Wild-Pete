7dc1440d7bcdb78c4af1cf6e7502e5dc2e8edeb0
Joseph Giap <145407086+JosephGiap123@users.noreply.github.com>
2025-10-14 04:00:36 -0500

    actually added the reloading, punching, death and sneak shoot

--- Assets/Scripts/Player/PlayerStates.cs
+++ Assets/Scripts/Player/PlayerStates.cs
@@ -5,5 +5,6 @@ public enum playerStates
     Idle, Run, Slide, Falling, Rising, Hurt, WallSlide, Attack, AerialAttack, Throw, Crouch,
     IdleWep, RisingWep, FallingWep, RunWep, SlideWep, HurtWep, WallSlideWep, CrouchWep,
     Melee1, Melee1_Recovery, Melee2, Melee2_Recovery, Melee3, Melee3_Recovery,
-    Dash, DashWep, RangedAttack, CrouchAttack
+    Dash, DashWep, RangedAttack, CrouchAttack, Punch1, Punch2, Death, DeathWep, Climb, 
+    CrouchRangedAttack, Reload
 }
