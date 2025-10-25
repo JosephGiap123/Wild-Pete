119a1e92feb78680c1b0e66de54b12351a7aa75b
Joseph Giap <145407086+JosephGiap123@users.noreply.github.com>
2025-10-25 01:10:16 -0500

    added dynamite WIP

--- Assets/Scripts/Projectiles/Bullet.cs
+++ Assets/Scripts/Projectiles/Bullet.cs
@@ -7,35 +7,50 @@ public class Bullet : MonoBehaviour
     [SerializeField] private LayerMask bulletDestroyMask;
     [SerializeField] private LayerMask enemyMask;
 
+    [SerializeField] private LayerMask staticMask;
     private Rigidbody2D rb;
-    private void Start(){
+    private void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
 
         SetStraightVelocity();
         SetDestroyTime();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision){
-        if(((1 << collision.gameObject.layer) & bulletDestroyMask) != 0){
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & bulletDestroyMask) != 0)
+        {
             //potentially spawn fx if not an enemy.
             //damage enemies
-            if (((1 << collision.gameObject.layer) & enemyMask) != 0) 
+            if (((1 << collision.gameObject.layer) & enemyMask) != 0)
             {
-                if(collision.gameObject != null){
+                if (collision.gameObject != null)
+                {
                     Debug.Log("Hit enemy");
                     collision.gameObject.transform.parent.gameObject.GetComponent<EnemyBase>().Hurt(2);
+                }
             }
-        }
+            else if (((1 << collision.gameObject.layer) & staticMask) != 0)
+            {
+                if (collision.gameObject != null)
+                {
+                    Debug.Log("Hit enemy");
+                    collision.gameObject.transform.parent.gameObject.GetComponent<BreakableStatics>().Damage(1);
+                }
+            }
             Destroy(gameObject);
         }
     }
 
     // Update is called once per frame
-    private void SetStraightVelocity(){
+    private void SetStraightVelocity()
+    {
         rb.linearVelocity = transform.right * bulletSpeed;
     }
 
-    private void SetDestroyTime(){
+    private void SetDestroyTime()
+    {
         Destroy(gameObject, bulletLifeTime);
     }
 }
---
+++ Assets/Scripts/Statics/Crate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Crate : BreakableStatics
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+}
