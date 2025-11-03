076194de666d2361624ccb61a57527a1ffcef837
Joseph Giap <145407086+JosephGiap123@users.noreply.github.com>
2025-11-02 22:44:55 -0600

    added some boss stuff, updated git ignore

--- Assets/Scripts/Enemy/Boss/Warden/WardenAI.cs
+++ Assets/Scripts/Enemy/Boss/Warden/WardenAI.cs
@@ -9,17 +9,46 @@ public class WardenAI : EnemyBase
 
     [Header("Movement Settings")]
     [SerializeField] protected float moveSpeed = 2f;
+    public bool isFacingRight = true;
 
     [Header("Combat Setting")]
+    public int phaseNum = 1;
     protected bool isDead = false;
     protected bool isInAir = false;
+    protected bool inAttackState = false;
     protected int isAttacking = 0;
+
+    [Header("Combat Stats")]
+    [SerializeField] protected int Melee1Dmg = 5;
+    [SerializeField] protected Vector2 Melee1Offset;
+    [SerializeField] protected Vector2 Melee1Size;
+    [SerializeField] protected Vector2 Melee1Knockback = new(5f, 2f);
+    [SerializeField] protected Vector2 Melee2Offset;
+    [SerializeField] protected Vector2 Melee2Size;
+    [SerializeField] protected int Melee2Dmg = 8;
+    [SerializeField] protected Vector2 Melee2Knockback = new(7f, 3f);
+    [SerializeField] protected int rangedDmg = 6;
+    [SerializeField] protected float rangedSpeed = 20f;
+    [SerializeField] protected float rangedLifeSpan = 4f;
+    [SerializeField] protected Vector2 RangedKnockback = new(4f, 1f);
+    [SerializeField] protected int Ultimate1Dmg = 7; //teleport slam, this dmg represents the initial slam part.
+    [SerializeField] protected Vector2 Ultimate1HitboxOffset;
+    [SerializeField] protected Vector2 Ultimate1HitboxSize;
+    [SerializeField] protected Vector2 Ultimate1Knockback = new(10f, -5f);
+    [SerializeField] protected int Ultimate2Dmg = 6; //ground slam, lasers come from ground.
+    [SerializeField] protected Vector2 Ultimate2Knockback = new(8f, 4f);
+    [SerializeField] protected int Ultimate3Dmg = 3; //laser beam, continuous hits.
+    [SerializeField] protected Vector2 Ultimate3Knockback = new(12f, 6f);
+    [SerializeField] protected Vector2 Ultimate3HitboxOffset;
+    [SerializeField] protected Vector2 Ultimate3HitboxSize;
+
 
     [Header("Combat References")]
     [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject laserPrefab;
     [SerializeField] private Transform projectileSpawnPoint;
     [SerializeField] private BoxCollider2D boxAttackHitbox;
-    [SerializeField] private AttackHitBoxGuard attackHitboxScript;
+    [SerializeField] private WardenAttackHitbox attackHitboxScript;
 
     private Transform player;
     private void OnEnable()
@@ -33,6 +62,24 @@ public class WardenAI : EnemyBase
     private void HandlePlayerSet(GameObject playerObj)
     {
         if (playerObj != null) this.player = playerObj.transform;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SetUpAttackHitboxes(1);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SetUpAttackHitboxes(6);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SetUpAttackHitboxes(3);
+        }
+        if (inAttackState) return;
+        AnimationControl();
     }
 
     public void ChangeAnimationState(string newState)
@@ -49,7 +96,7 @@ public class WardenAI : EnemyBase
             ChangeAnimationState("Death");
             return;
         }
-        else if (isAttacking != 0)
+        else if (inAttackState)
         {
             switch (isAttacking)
             {
@@ -70,6 +117,12 @@ public class WardenAI : EnemyBase
                     break;
                 case 6:
                     ChangeAnimationState("Ultimate3"); //laser beam
+                    break;
+                case 7: //Attack1 Recovery
+                    ChangeAnimationState("Attack1Recovery");
+                    break;
+                case 8: //Slam Recovery
+                    ChangeAnimationState("Ultimate2Recovery");
                     break;
                 default:
                     break;
@@ -99,4 +152,120 @@ public class WardenAI : EnemyBase
         }
     }
 
+    public void FlipSprite()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        projectileSpawnPoint.localRotation = Quaternion.Euler(0, 0, isFacingRight ? 0 : 180);
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+    public void EndAttack()
+    {
+        isAttacking = 0;
+    }
+
+    public void ChangeAttackNumber(int newAttackNum)
+    {
+        isAttacking = newAttackNum;
+    }
+
+    public void EndAttackState()
+    {
+        inAttackState = false;
+    }
+
+    public void ActivateHitbox()
+    {
+        attackHitboxScript.ActivateBox();
+    }
+
+    public void DisableHitbox()
+    {
+        attackHitboxScript.DisableHitbox();
+    }
+
+    public void AddVelocity(float addedVelocity)
+    {
+        float direction = isFacingRight ? 1f : -1f;
+        rb.linearVelocity += new Vector2(direction * addedVelocity, 0f);
+    }
+    public void ZeroVelocity()
+    {
+        rb.linearVelocity = Vector2.zero;
+    }
+
+    //in phase 1, melee 1 chains into melee 1 recovery
+    // in phase 2 onwards, melee 1 chains straight into melee 2
+    //in phase 2 or higher, all ultimates are available.
+    //in phase 3, the warden will chain 3 ultimate 1's in a row.
+    //in phase 3, the warden will have faster movement speed
+    public void SetUpAttackHitboxes(int attackNum)
+    {
+        isAttacking = attackNum;
+        inAttackState = true;
+        switch (attackNum)
+        {
+            case 1: //melee chain 1
+                attackHitboxScript.ChangeHitboxBox(new Vector2(1.0f, 0.5f), new Vector2(2.0f, 1.5f), Melee1Dmg, Melee1Knockback);
+                ChangeAnimationState("Attack1");
+                break;
+            case 2: // melee chain 2
+                attackHitboxScript.ChangeHitboxBox(new Vector2(1.5f, 0.5f), new Vector2(2.5f, 1.5f), Melee2Dmg, Melee2Knockback);
+                ChangeAnimationState("Attack2");
+                break;
+            case 3:
+                ChangeAnimationState("RangedAttack");
+                break;
+            case 4: // ultimate 1 (slam down)
+                attackHitboxScript.ChangeHitboxBox(Ultimate1HitboxOffset, Ultimate1HitboxSize, Ultimate1Dmg, Ultimate1Knockback);
+                ChangeAnimationState("Ultimate1");
+                break;
+            case 5:
+                ChangeAnimationState("Ultimate2");
+                break;
+            case 6: //ultimate 3 (laser beam)
+                attackHitboxScript.ChangeHitboxBox(Ultimate3HitboxOffset, Ultimate3HitboxSize, Ultimate3Dmg, Ultimate3Knockback);
+                ChangeAnimationState("Ultimate3");
+                break;
+            case 7:
+                ChangeAnimationState("Attack1Recovery");
+                break;
+            case 8:
+                ChangeAnimationState("Ultimate2Recovery");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Ult2LaserSpawn(int count) //summons lasers that "chase" the player every 1 second.
+    {
+
+    }
+
+    public void EndMelee1Chain()
+    {
+        if (phaseNum == 1)
+        {
+            SetUpAttackHitboxes(7);
+        }
+        else
+        {
+            SetUpAttackHitboxes(2); //chain into melee 2
+        }
+    }
+
+    public void EndUltimate1And2()
+    {
+        SetUpAttackHitboxes(8);
+    }
+
+    public void InstBullet()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+        GuardBullet projScript = projectile.GetComponent<GuardBullet>();
+        projScript.Initialize(rangedDmg, rangedSpeed, rangedLifeSpan);
+        return;
+    }
 }
