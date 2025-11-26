deb618439a673d2e1b5d1e4e4313180658d1390f
mellowsss <yousifsaieb@gmail.com>
2025-11-25 22:57:13 -0600

    life is good
    fixed the first stage fixed bugs and stuff and um  fixed the  lights and sfx

--- Assets/Run.cs
+++ Assets/Run.cs
@@ -4,7 +4,7 @@ public class Run : StateMachineBehaviour
 {
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
+    //{ i fixed it
     //    
     //}
 
