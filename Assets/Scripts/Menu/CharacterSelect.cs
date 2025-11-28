4a2f46368af5d987a22c1774094e94cf0855b4e0
ttfamm <tfamm022@gmail.com>
2025-11-27 22:33:29 -0600

    moved cutscene

--- Assets/Scripts/Menu/CharacterSelect.cs
+++ Assets/Scripts/Menu/CharacterSelect.cs
@@ -7,11 +7,17 @@ public class CharacterSelect : MonoBehaviour
 
   [SerializeField] private SwapSceneEventSO sceneSwapEventSO;
   [SerializeField] private IntEventSO selectedCharacterEventSO;
+  public CutSceneManager cutsceneManager;
+  public GameObject mainMenuCanvas;
+  public GameObject charSelectCanvas;
   public void chooseCharacter(int charNum)
   {
     charNum = Mathf.Clamp(charNum, 1, 2);
     selectedCharacterEventSO.RaiseEvent(charNum);
-    StartCoroutine(LoadSceneCoroutine());
+    charSelectCanvas.SetActive(false);
+    mainMenuCanvas.SetActive(false);
+    // StartCoroutine(LoadSceneCoroutine());
+    cutsceneManager.BeginCutscene();
     //then load scene.
   }
 
