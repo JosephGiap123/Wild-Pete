7144652caac7a7d8ec8fc6d5f7c8982e094b3a98
ttfamm <tfamm022@gmail.com>
2025-11-29 01:18:58 -0600

    dust particles

--- Assets/Scripts/Menu/CharacterSelect.cs
+++ Assets/Scripts/Menu/CharacterSelect.cs
@@ -8,14 +8,14 @@ public class CharacterSelect : MonoBehaviour
   [SerializeField] private SwapSceneEventSO sceneSwapEventSO;
   [SerializeField] private IntEventSO selectedCharacterEventSO;
   public CutSceneManager cutsceneManager;
-  public GameObject mainMenuCanvas;
+  // public GameObject mainMenuCanvas;
   public GameObject charSelectCanvas;
   public void chooseCharacter(int charNum)
   {
     charNum = Mathf.Clamp(charNum, 1, 2);
     selectedCharacterEventSO.RaiseEvent(charNum);
     charSelectCanvas.SetActive(false);
-    mainMenuCanvas.SetActive(false);
+    // mainMenuCanvas.SetActive(false);
     // StartCoroutine(LoadSceneCoroutine());
     cutsceneManager.BeginCutscene();
     //then load scene.
